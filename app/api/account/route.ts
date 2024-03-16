1d1e6e48dcdca67c47ad96fb484ca0aa79ac9899
dingjoys <dingjoys@126.com>
2024-03-17 06:59:06 +0800

    test

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -15,7 +15,7 @@ const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
 const bundlerUrl = `https://api.pimlico.io/v1/sepolia/rpc?apikey=${apiKey}`
 
 const publicClient = createPublicClient({
-    transport: http("https://rpc.ankr.com/eth_sepolia"),
+    transport: http("https://maximum-spring-daylight.base-sepolia.quiknode.pro/f80c89e1e8f03bdb4eea77aa68bf8546d8862cc5/"),
 })
 
 const paymasterClient = createPimlicoPaymasterClient({
@@ -25,7 +25,7 @@ const paymasterClient = createPimlicoPaymasterClient({
 export async function POST(req: NextRequest): Promise<NextResponse> {
     const body: FrameRequest = await req.json();
     const { isValid, message } = await getFrameMessage(body, { neynarApiKey: process.env.NEYNAR_API_KEY! });
-    console.log("isvalid", isValid, message, body)
+    console.log("isvalid", isValid, message?.button)
     if (!isValid) {
         return new NextResponse('Invalid Frame message', { status: 400 });
     }
@@ -35,7 +35,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     }
 
     const accountAddress = message.interactor.verified_accounts[0] as Address;
-
+    console.log("accountAddress", accountAddress)
     // send transaction
     const account = await privateKeyToSafeSmartAccount(publicClient, {
         privateKey: privateKey as Address,
@@ -55,24 +55,24 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
     const gasPrices = await smartAccountClient.getUserOperationGasPrice()
 
-    const callData = await account.encodeCallData({
-        to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-        data: "0x1234",
-        value: BigInt(0)
-    })
+    // const callData = await account.encodeCallData({
+    //     to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+    //     data: "0x1234",
+    //     value: BigInt(0)
+    // })
 
-    const userOperation = await smartAccountClient.prepareUserOperationRequest({
-        userOperation: {
-            callData
-        },
-    })
+    // const userOperation = await smartAccountClient.prepareUserOperationRequest({
+    //     userOperation: {
+    //         callData
+    //     },
+    // })
 
-    userOperation.signature = await account.signUserOperation(userOperation)
+    // userOperation.signature = await account.signUserOperation(userOperation)
 
-    const userOpHash = await smartAccountClient.sendUserOperation({
-        userOperation,
-        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
-    })
+    // const userOpHash = await smartAccountClient.sendUserOperation({
+    //     userOperation,
+    //     entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+    // })
 
     return new NextResponse(
         getFrameHtmlResponse({
